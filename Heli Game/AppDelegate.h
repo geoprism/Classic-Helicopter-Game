b6e10e60ceeb595dc3292eb00aa50fb1ea05ee3d
Brian Wong <wongbm2@uci.edu>
2016-07-11 18:01:31 -0700

    uploading heli game

---
+++ Heli Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Heli Game
+//
+//  Created by Brian Wong on 12/24/15.
+//  Copyright © 2015 BW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Heli Game/ViewController.h
@@ -0,0 +1,64 @@
+//
+//  ViewController.h
+//  Heli Game
+//
+//  Created by Brian Wong on 12/24/15.
+//  Copyright © 2015 BW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int Y;
+BOOL Start;
+int RandomPosition;
+int ScoreNumber;
+int HighScore;
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UILabel *Intro1;
+    IBOutlet UILabel *Intro2;
+    IBOutlet UILabel *Intro3;
+    
+    IBOutlet UIImageView *Heli;
+    
+    NSTimer *timer;
+    
+    IBOutlet UIImageView *Obstacle1;
+    IBOutlet UIImageView *Obstacle2;
+    IBOutlet UIImageView *Bottom1;
+    IBOutlet UIImageView *Bottom2;
+    IBOutlet UIImageView *Bottom3;
+    IBOutlet UIImageView *Bottom4;
+    IBOutlet UIImageView *Bottom5;
+    IBOutlet UIImageView *Bottom6;
+    IBOutlet UIImageView *Bottom7;
+    IBOutlet UIImageView *Top1;
+    IBOutlet UIImageView *Top2;
+    IBOutlet UIImageView *Top3;
+    IBOutlet UIImageView *Top4;
+    IBOutlet UIImageView *Top5;
+    IBOutlet UIImageView *Top6;
+    IBOutlet UIImageView *Top7;
+    
+    
+    IBOutlet UILabel *ScoreHolder;
+    IBOutlet UILabel *Score;
+    
+    NSTimer *Scorer;
+    
+
+    
+
+
+}
+
+-(void)HeliMove;
+-(void)Collision;
+-(void)EndGame;
+-(void)NewGame;
+-(void)Scoring;
+
+
+@end
+
